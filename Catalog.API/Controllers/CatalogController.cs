4d9b7a03866e547dfe643ba11b9cab319f4730ec
Karine Ribeiro <karine.yasmin@outlook.com>
2025-05-09 16:11:04 -0300

    Add some controllers

--- Catalog.API/Controllers/CatalogController.cs
+++ Catalog.API/Controllers/CatalogController.cs
@@ -1,10 +1,55 @@
+using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Catalog.API.Controllers
+namespace Catalog.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CatalogController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CatalogController : ControllerBase
+    private readonly IProductRepository _repository;
+
+    public CatalogController(IProductRepository repository)
     {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        var products = await _repository.GetProducts();
+        return Ok(products);
+    }
+
+
+    [HttpGet("{id:length(24)}", Name = "GetProduct")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(Product), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<Product>> GetProductById(string id)
+    {
+        var product = await _repository.GetProduct(id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+        return Ok(product);
+    }
+
+
+    [Route("[action/{category}], Name = GetProductByCategory")]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Product>))]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+    {
+        if (category is null) return BadRequest("Invalid category");
+
+        var products = await _repository.GetProductByCategory(category);
+
+        return Ok(products);
+
     }
 }
